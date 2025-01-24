b517b50f359890e25c188b0476150acd8df279e1
Konstantin Yavichev <kyavichev@gmail.com>
2025-01-23 21:56:51 -0800

    Cleaned up RotateArray tests

--- CodeProblems/Assets/Tests/RotateArrayTests.cs
+++ CodeProblems/Assets/Tests/RotateArrayTests.cs
@@ -1,38 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 
 
 public class RotateArrayTests
 {
-    public class TestData
-    {
-        public int[] nums;
-        public int k;
-        public int[] result;
-
-        public TestData(int[] newNums, int newK, int[] newResult)
-        {
-            nums = newNums;
-            k = newK;
-            result = newResult;
-        }
-    }
-
+    [TestCase("[1,2,3,4,5,6,7]", 3, "[5,6,7,1,2,3,4]")]
+    [TestCase("[-1,-100,3,99]", 2, "[3,99,-1,-100]")]
     [Test]
-    public void TestRotateArray()
+    public void TestRotateArray(string numsString, int k, string expectedResultString)
     {
-        List<TestData> inputData = new List<TestData>()
-        {
-            new TestData(new int[]{1,2,3,4,5,6,7}, 3, new int[]{5,6,7,1,2,3,4}),
-            new TestData(new int[]{-1,-100,3,99}, 2, new int[]{3,99,-1,-100}),
-        };
-
-        foreach (var test in inputData)
-        {
-            RotateArraySolution.Rotate(test.nums, test.k);
-            Assert.AreEqual(test.nums, test.result);
-        }
+        int[] nums = InputStringParser.ParseIntArray(numsString);
+        RotateArraySolution.Rotate(nums, k);
+        int[] expectedResult = InputStringParser.ParseIntArray(expectedResultString);
+        Assert.AreEqual(nums, expectedResult);
     }
 }
