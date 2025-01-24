8b27c295ad91d173eabc78d1c22792ba3a4f5486
Konstantin Yavichev <kyavichev@gmail.com>
2025-01-24 10:09:24 -0800

    Cleaned up QuickSort tests

--- CodeProblems/Assets/Tests/QuickSortTests.cs
+++ CodeProblems/Assets/Tests/QuickSortTests.cs
@@ -6,51 +6,62 @@ using UnityEngine;
 
 public class QuickSortTests
 {
+    [TestCase("[8,875,7,9,764,55]")]
+    [TestCase("[18,2,347,6689,4,255,254,256,7,100,13]")]
+    [TestCase("[1892,2000,3417,6689,4200,6255,6254,6256,7000,1003,1399]")]
     [Test]
-    public void TestQuickSort()
+    public void TestQuickSort(string inputString)
     {
-        // Quick Sort test
-        int[][] ints =
+        var nums = InputStringParser.ParseIntArray(inputString);
+
+        var sortedNumsList = new List<int>(nums);
+        sortedNumsList.Sort();
+        var sortedNums = sortedNumsList.ToArray();
+
+        QuickSort.Sort(nums);
+
+        Assert.AreEqual(nums[0], sortedNums[0]);
+        Assert.AreEqual(nums, sortedNums);
+    }
+
+
+    [Test]
+    public void TestQuickSortRandom()
+    {
+        // Generate new data
+        int size = UnityEngine.Random.Range(5, 10);
+        List<int[]> intsList = new List<int[]>();
+        for (int i = 0; i < size; i++ )
         {
-            new int[] { 8, 875, 7, 9, 764, 55 },
-            new int[] { 18, 2, 347, 6689, 4, 255, 254, 256, 7, 100, 13 },
-            new int[] { 1892, 2000, 3417, 6689, 4200, 6255, 6254, 6256, 7000, 1003, 1399 },
-        };
+            var intList = new List<int>();
+            int listSize = UnityEngine.Random.Range(10, 20);
+            for(int j = 0; j < listSize; j++)
+            {
+                intList.Add(UnityEngine.Random.Range(0, 10000));
+            }
+            intsList.Add(intList.ToArray());
+        }
+        int[][] ints = intsList.ToArray();
 
         // Create a duplicate array and sort it using Linq
         List<int[]> sortedList = new List<int[]>();
-        for(int i = 0; i < ints.Length; i++)
+        for (int i = 0; i < ints.Length; i++)
         {
             var list = new List<int>(ints[i]);
             list.Sort();
             sortedList.Add(list.ToArray());
         }
-        sortedList.Sort((int[] a, int[] b) => { return a[0].CompareTo(b[0]); } );
+        sortedList.Sort((int[] a, int[] b) => { return a[0].CompareTo(b[0]); });
         int[][] sorted = sortedList.ToArray();
 
         // Sort each individual array
         for (int i = 0; i < ints.Length; i++)
         {
-            Debug.Log($"Before: {ToStringUtils.ArrayToString(ints[i])}");
             QuickSort.Sort(ints[i]);
-            Debug.Log($"After: {ToStringUtils.ArrayToString(ints[i])}");
         }
 
         // Sort arrays using the first integer in each array
         QuickSort.Sort(ints, (int[] a, int[] b) => { return a[0].CompareTo(b[0]); });
-        for (int i = 0; i < ints.Length; i++)
-        {
-            Debug.Log($"{i}: {ToStringUtils.ArrayToString(ints[i])}");
-        }
-
-        Assert.AreEqual(ints[0][0], 2);
-        Assert.AreEqual(ints[0][^1], 6689);
-
-        Assert.AreEqual(ints[1][0], 7);
-        Assert.AreEqual(ints[1][^1], 875);
-
-        Assert.AreEqual(ints[2][0], 1003);
-        Assert.AreEqual(ints[2][^1], 7000);
 
         Assert.AreEqual(ints[0][0], sorted[0][0]);
         Assert.AreEqual(ints[0], sorted[0]);
